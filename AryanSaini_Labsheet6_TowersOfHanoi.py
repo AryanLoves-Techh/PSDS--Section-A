275f7f49de6f3ee225d815d7475c7fd6665fd39e
Aryan Saini <aryanintech26@gmail.com>
2026-09-07 10:16:41 +0530

    Implement Towers of Hanoi algorithm

---
+++ AryanSaini_Labsheet6_TowersOfHanoi.py
@@ -0,0 +1,13 @@
+def towers_of_hanoi(n ,source, target, auxiliary):
+    if n == 1:
+        print(f'Move disk 1 from {source} to {target}')
+        return
+
+    towers_of_hanoi(n - 1, source, auxiliary, target)
+
+    print(f'Move disk {n} from {source} to {target}')
+
+    towers_of_hanoi(n-1, auxiliary, target, source)
+
+disks = int(input("Enter the no. of disks"))
+towers_of_hanoi(disks, 'A', 'C', 'B')
